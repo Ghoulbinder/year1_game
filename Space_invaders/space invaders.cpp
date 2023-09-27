a83b75c5d41ba7c67f18a57f82eb818be06029a1
Romeo MCdonald <romeomcdonald24@gmail.com>
2023-09-27 12:19:53 +0100

    sfml update

--- Space_invaders/space invaders.cpp
+++ Space_invaders/space invaders.cpp
@@ -1,6 +1,23 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+
+using namespace std;
+using namespace sf;
+sf::Texture spritesheet;
+sf::Sprite invader;
 
 
+void Load() {
+    if (!spritesheet.loadFromFile("C:/Users/romeo/year1_game/res/img/invaders_sheet.png")) {
+        cerr << "Failed to load spritesheet!" << std::endl;
+    }
+    invader.setTexture(spritesheet);
+    invader.setTextureRect(sf::IntRect(sf::Vector2(0, 0), sf::Vector2(32, 32)));
+}
+
+void Render(RenderWindow& window) {
+    window.draw(invader);
+}
 
 int main(){
     sf::RenderWindow window(sf::VideoMode({ 200, 200 }), "SFML works!");
